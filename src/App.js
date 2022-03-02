8fc5d47f148de6511665eb9a97dbd47e94565e94
Barun Ojha <barun.ojha@gurzu.net>
2022-03-02 15:37:08 +0545

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <Container className="App vh-100 d-flex justify-content-center align-items-center">
+      <h1>HELLO WORLD!</h1>
+    </Container>
+  );
+}
+
+export default App;
